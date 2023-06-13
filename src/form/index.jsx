72bdd27226d18b58e08d2577d94f80d5b3c7d9e0
dardirii <dimasdardiri@gmail.com>
2023-06-14 02:07:52 +0700

    Form

---
+++ src/form/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FormElement from "./Form.jsx";
+
+export default class Form extends React.Component{
+    render(){
+        return(
+            <FormElement />
+        )
+        }
+}
